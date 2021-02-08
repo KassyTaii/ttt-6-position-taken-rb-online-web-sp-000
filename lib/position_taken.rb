5912822ca0e1478efd70e108ec86bd5003705e76
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-08 01:40:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == " " || board[index] == nil
     taken = false
 elsif board[index] == "X" || board[index] == "O"|| board[index] == true
-  taken = true 
+  taken = true
 end
 end
