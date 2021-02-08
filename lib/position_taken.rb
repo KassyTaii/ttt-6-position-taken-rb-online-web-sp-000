0c453a0232b3710ee84e0f7eecff4bbff50b0322
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-08 01:17:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-end
-def move(board, index, value="X")
-  board[index] = value
-end
+index = 0
+
 position_taken?(board, index)
